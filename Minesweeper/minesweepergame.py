22d46e1e34a83bc9d5da623bb1714f0552febe9d
IshaanP <90219558+BesottedP@users.noreply.github.com>
2024-06-11 02:29:52 -0400

    Initital board setup

---
+++ Minesweeper/minesweepergame.py
@@ -0,0 +1,62 @@
+import random as rand
+import numpy as np
+
+NUM_MINES = 91
+
+def place_mines(board, start_row, start_col):
+    num_bombs = 0
+    while num_bombs != NUM_MINES:
+        rand_row = rand.randint(0,9)
+        rand_col = rand.randint(0,9)
+        if(rand_row >= start_row -1 and rand_row <= start_row + 1 and
+           rand_col >= start_col -1 and rand_col <= start_col + 1):
+            continue
+        if(board[rand_row][rand_col] != -1):
+            board[rand_row][rand_col] = -1
+            num_bombs += 1
+
+def set_num_mines(board):
+    for row in range(0, 10):
+        for col in range(0, 10):
+            num_adj_mines = 0
+            for adj_row in range(-1,2):
+                for adj_col in range(-1,2):
+                    check_col = col + adj_col
+                    check_row = row + adj_row
+                    if((check_col == -1 or check_row == -1)
+                       or (check_col == 10 or check_row == 10)
+                        or (adj_col == 0 and adj_row == 0)):
+                        continue
+                    elif(board[check_row][check_col] == -1):
+                        num_adj_mines += 1
+            if(board[row][col] != -1):
+                board[row][col] = num_adj_mines
+
+def reveal_empty_tiles(master_board, player_board, row, col):
+    for adj_row in range(-1,2):
+            for adj_col in range(-1,2):
+                check_row = row + adj_row
+                check_col = col + adj_col
+                if((check_col == 0 or check_row == 0)
+                       or (check_col == 11 or check_row == 11)):
+                        continue
+                if(player_board[check_row-1][check_col-1] == '*' and (master_board[check_row-1][check_col-1] != -1)):
+                    player_board[check_row-1][check_col-1] = master_board[check_row-1][check_col-1]
+                    if(player_board[check_row-1][check_col-1] == '0'):
+                        reveal_empty_tiles(master_board, player_board, check_row, check_col)
+
+master_board = np.zeros((10, 10))
+player_board = np.full((10, 10), "*")
+start_row, start_column = map(int, input("Enter a row and column to start ").split())
+master_board[start_row-1 , start_column-1] = -2
+place_mines(master_board, start_row-1, start_column-1)
+set_num_mines(master_board)
+print(master_board)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+reveal_empty_tiles(master_board, player_board, start_row, start_column)
+player_board[start_row-1, start_column-1] = master_board[start_row-1 , start_column-1]
+print(player_board)
+
+
+#TODO array number one should shows all the information on the board, and is created after the player makes the first move
+#In each tile it should show the number of bombs touching
