45d3f30789c40a47e9ff06ab4dbad77f4ce32702
IshaanP <besotted55@gmail.com>
2024-09-08 21:26:57 -0400

    Added Vectorized Environments

--- Minesweeper/minesweeperlearn.py
+++ Minesweeper/minesweeperlearn.py
@@ -1,16 +1,19 @@
-import gymnasium as gym
-from stable_baselines3 import A2C, PPO
+from gymnasium.envs.registration import register
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
 from minesweeperenv import MinesweeperEnv
 import os
-import time
 
 model_type = "PPO"
 
-env = MinesweeperEnv(render_mode="human")
+register(id='Minesweeper-v0', entry_point='minesweeperenv:MinesweeperEnv')
+env = make_vec_env("Minesweeper-v0", n_envs=16)
+
+# env = MinesweeperEnv(render_mode="human")
 env.reset()
 
-models_dir = f"Minesweeper/models/att6"
-logdir = f"Minesweeper/logs/att6"
+models_dir = f"Minesweeper/models/Easy"
+logdir = f"Minesweeper/logs/Easy"
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
@@ -18,15 +21,15 @@ if not os.path.exists(models_dir):
 if not os.path.exists(logdir):
     os.makedirs(logdir)
 
-# model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir, device="cuda", n_steps=8192, batch_size=256)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir, device="cuda")
 
-custom_objects = { 'learning_rate': 0.003 }
-model_path = f"{models_dir}/18700000.zip"
-model = PPO.load(model_path, env=env, device ="cuda", custom_objects=custom_objects)
+# custom_objects = { 'learning_rate': 0.0004 }
+model_path = f"{models_dir}/2900000.zip"
+model = PPO.load(model_path, env=env, device ="cuda")
 
-TIMESTEPS = 100000
-for i in range(1, 1000):
+TIMESTEPS = 1000000
+for i in range(1, 100000):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=model_type)
-    model.save(f"{models_dir}/{TIMESTEPS*i}")
+    model.save(f"{models_dir}/{(TIMESTEPS*i)+2900000}")
 
 env.close()
